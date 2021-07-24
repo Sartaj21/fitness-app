ca46acb5a8c92a84a27f1d06cf17fe3c4a59a2b9
Sartaj GIll <53071012+Sartaj21@users.noreply.github.com>
2021-07-24 14:58:14 -0400

    Updated to havegraph libary

--- src/components/try.js
+++ src/components/try.js
@@ -1,11 +1,12 @@
 import React from "react";
 import './try.css'
+import {Bar} from 'react-chartjs-2'
 
 
 
 function Try(props){
         return(
-            <div class="load_screen">
+            <div className="load_screen">
                 <h2>To get a Head Start on the Future Sroll Down</h2>
             </div>
         );
